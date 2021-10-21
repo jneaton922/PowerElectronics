08380239ad158b45c2e7e99c69cd437955d6ccaa
Josh Eaton <jeaton14@jh.edu>
2021-10-20 21:46:22 -0400

    Add Module 6 stuff, small signal modeling

--- Module6/MATLAB/lsm.m
+++ Module6/MATLAB/lsm.m
@@ -6,17 +6,17 @@ V = 12;         % outpout voltage
 Rg = 140e-3;
 fsw = 100e3;
 
-D = V/Vg;
-
 Ts = 1/fsw;
 Iout = Pout/V;
+
+D = (V)/(Vg-(Iout*Rg))
 
 vrip = 0.1;
 irip = 0.05*Iout;
 
 Rl = V/Iout;
 
-L = (Vg - V) / 
+L = ((Vg - V) / (2*irip)) * D * Ts;
 
-C = (irip*Ts)/(8*vrip);
+C = ( irip * Ts ) / (8 * vrip);
 
